29996e10e28acd8f1b77225b20ef077e7cc67d85
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-21 15:25:57 +1000

    button push and basic css

---
+++ app.js
@@ -0,0 +1,14 @@
+// Temporary hardcoded data
+
+
+document.querySelector('.turn-button').addEventListener('click', buttonSound);
+
+
+function buttonSound(){
+    const sound = document.querySelector('#audio');
+    sound.play();
+}
+
+function playSong(){
+    const song = document.querySelector()
+}
