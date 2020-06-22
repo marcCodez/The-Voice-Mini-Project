fb5a594c7b4c883278c156537ccd0bad8378bda3
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-22 17:33:42 +1000

    react sound effects

--- app.js
+++ app.js
@@ -9,10 +9,18 @@ const data = [
 	},
 	{
 		name: 'James Arthur',
-		age: 23,
+		age: 24,
 		location: 'UK',
         song: 'Frankie Vallie - Cant Take my Eyes Off you',
         link:'https://www.youtube.com/embed/JUeEcdS-aa0?start=12'
+       
+    },
+    {
+		name: 'Francisco Martin',
+		age: 22,
+		location: 'America',
+        song: 'Harry Styles - Falling',
+        link:'https://www.youtube.com/embed/b4IDCCJDCTg?start=2'
        
 	},
 	
@@ -24,13 +32,20 @@ document.querySelector('.turn-button').addEventListener('click', buttonSound);
 // Create event for next button
 document.querySelector('.next').addEventListener('click', nextContestant);
 
+document.querySelector('.negative').addEventListener('click', negativeReact);
+
+document.querySelector('.positive').addEventListener('click', positiveReact);
+
 
 // pass the data in the profileIterator function
 const contestants = profileIterator(data);
 
-
 // Call first profile
+// nextContestant();
 nextContestant();
+
+
+
 
 
 
@@ -38,7 +53,7 @@ nextContestant();
 // Next profile Display
 function nextContestant() {
     const currentContestant = contestants.next().value;
-   
+
  
     // Add a condition if it finishes looping through the array
     if(currentContestant !== undefined){
@@ -47,23 +62,28 @@ function nextContestant() {
     <div id="imageDisplay"><img class="anon"
                             src="media/anon.png"></div>
     <ul class="list-group">
-        <li class="list-group-item" id="name">Name: ? 
+        <li class="list-group-item" id="name">Name: ${currentContestant.name}
         </li>
-        <li class ="list-group-item" id="age">Age: ?
+        <li class ="list-group-item" id="age">Age: ${currentContestant.age}
         </li>
-        <li class ="list-group-item" id="location">Location: ?
+        <li class ="list-group-item" id="location">Location: ${currentContestant.location}
         </li>
         <li class ="list-group-item">Song: ${currentContestant.song} 
         </li>
     </ul>
     `;
+   
+  
     } else {
         // No more contestants
         window.location.reload();
     }
-    return {
-        contestant: currentContestant
-    }
+
+  
+
+   
+    
+    
 
     // document.querySelector('#imageDisplay').innerHTML = `<img
     // src="${currentContestant.image}">`;
@@ -80,7 +100,7 @@ return {
         return nextIndex < contestants.length ?
         {value: contestants[nextIndex++], done: false} :
         {done: true}
-    }
+    } 
 };
 
 }
@@ -88,15 +108,55 @@ return {
 
 
 function buttonSound(){
-
+ 
     const sound = document.querySelector('#audio');
     sound.play();
     document.querySelector(".anon").style.display = 'none'
-    document.querySelector("iFrame").style.display='block';
-    document.querySelector("#name").innerText=`Name: ${currentContestant.name}`;
-    document.querySelector("#age").innerText=`Age: ${currentContestant.age}`;
-    document.querySelector("#location").innerText=`Location: ${currentContestant.location}`;
+    document.querySelector("iframe").style.display='block';
+    document.querySelector("#name").style.display='inline';
+    document.querySelector("#age").style.display=`inline`;
+    document.querySelector("#location").style.display=`inline`;
     // document.querySelector('#imageDisplay').innerHTML = `<img
     // src="${currentContestant.image}">`;
+}
+
+
+function negativeReact(){
+     //An array to house all of the URLs of your sounds
+     var sounds = [ "audio/boring.mp3",
+     "audio/what.mp3",
+    "audio/garbage.mp3",
+"audio/no.mp3",
+"audio/stop.mp3"];
+
+//This line will select a random sound to play out of your provided URLS
+var soundFile = sounds[Math.floor(Math.random()*sounds.length)];
+
+//Find the player element that you created and generate an embed file to play the sound within it
+document.querySelector("#player").src= soundFile ;
+const negReact = document.querySelector("#player");
+negReact.play();
+
+
+}
+
+function positiveReact(){
+    //An array to house all of the URLs of your sounds
+    const sounds = [ "audio/clap.mp3",
+"audio/unbev.mp3",
+"audio/greatness.mp3",
+"audio/fav.mp3",
+"audio/winner.mp3",
+"audio/genius.mp3"
+];
+
+//This line will select a random sound to play out of your provided URLS
+const soundFile = sounds[Math.floor(Math.random()*sounds.length)];
+
+//Find the player element that you created and generate an embed file to play the sound within it
+document.querySelector("#player2").src=soundFile;
+const posReact = document.querySelector("#player2");
+posReact.play();
+    
 }
 
